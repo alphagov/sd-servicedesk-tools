2b92a6b7fd5ef68cec9668c11827fa9fa8659c69
adriandurran <adriandurran@users.noreply.github.com>
2018-06-15 13:26:28 +0100

    feat: added moment added pendingtickets

--- client/src/components/newstarts/PendingTickets.js
+++ client/src/components/newstarts/PendingTickets.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
-import { Grid, Header, Card } from 'semantic-ui-react';
+import { Header, Card } from 'semantic-ui-react';
 import { connect } from 'react-redux';
+import Moment from 'react-moment';
+
+import { selectNewStartTicketsPending } from '../../reducers/selectors';
 
 class PendingTickets extends Component {
+  renderPendingStarts() {
+    const { pending } = this.props;
+    return pending.map(tkt => {
+      return (
+        <Card key={tkt.id}>
+          <Card.Content>
+            <Card.Header>{tkt.id}</Card.Header>
+            <Card.Meta>Client: {tkt.displayClient}</Card.Meta>
+            <Card.Description>{tkt.shortDetail}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            Last updated <Moment fromNow>{tkt.lastUpdated}</Moment>
+          </Card.Content>
+        </Card>
+      );
+    });
+  }
+
   render() {
-    return <div>Pending Tickets</div>;
+    return (
+      <div>
+        <Header as="h3" textAlign="center">
+          Pending New Starter Tickets
+        </Header>
+        <Card.Group itemsPerRow={4}>{this.renderPendingStarts()}</Card.Group>
+      </div>
+    );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    pending: selectNewStartTicketsPending(state)
+  };
+};
+
+PendingTickets = connect(mapStateToProps)(PendingTickets);
 
 export default PendingTickets;
