9d1a7112f9f47515d4e58b4c4e97f0fe111129ba
Adrian Durran <adrian@intellidroid.eu>
2018-06-20 15:44:46 +0100

    feat: added ticket notes to the detail

--- client/src/components/newstarts/StarterTicketDetails.js
+++ client/src/components/newstarts/StarterTicketDetails.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { isEmpty } from 'lodash';
+import { Grid, Form, Header, Item } from 'semantic-ui-react';
+import Moment from 'react-moment';
+import { removeHTML } from '../../utils/stringManip';
 
 import { selectTicket } from '../../reducers/selectors/tickets';
 
 class StarterTicketDetails extends Component {
+  renderTicketNotes() {
+    const { ticket } = this.props;
+
+    if (isEmpty(ticket) || ticket.notes.length === 0) {
+      return (
+        <Item>
+          <Item.Header>No ticket notes</Item.Header>
+        </Item>
+      );
+    }
+
+    return ticket.notes.map(note => {
+      let noteText = removeHTML(note.mobileNoteText);
+      return (
+        <Item key={note.id}>
+          <Item.Content>
+            <Item.Header>
+              {note.isTechNote ? 'Analyst note' : 'Client note'} -{' '}
+              <Moment fromNow>{note.date}</Moment>
+            </Item.Header>
+            <Item.Description>{noteText}</Item.Description>
+          </Item.Content>
+        </Item>
+      );
+    });
+  }
+
   render() {
-    return <div>Ticket Details</div>;
+    return (
+      <div>
+        <Header as="h3" textAlign="center">
+          Ticket Details
+        </Header>
+        <Grid>
+          <Grid.Row columns={2}>
+            <Grid.Column>Ticket details</Grid.Column>
+            <Grid.Column>
+              <Header as="h4" textAlign="center">
+                Notes
+              </Header>
+              <Item.Group>{this.renderTicketNotes()}</Item.Group>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>Updatable details in here</Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    );
   }
 }
 
