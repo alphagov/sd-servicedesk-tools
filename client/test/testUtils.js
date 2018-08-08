fd3d1f5cfc943f2a227570547afe5876e9e793c2
adriandurran <adriandurran@users.noreply.github.com>
2018-08-08 15:42:29 +0100

    fix: grrrrr watchman

--- client/test/testUtils.js
+++ client/test/testUtils.js
@@ -1,35 +1,35 @@
-// import checkPropTypes from 'check-prop-types';
+import checkPropTypes from 'check-prop-types';
 
-// import rootReducer from '../reducers';
-// import { middlewares } from '../utils/configureStore';
-// import { createStore, applyMiddleware } from '../../node_modules/redux';
+import rootReducer from '../reducers';
+import { middlewares } from '../utils/configureStore';
+import { createStore, applyMiddleware } from '../../node_modules/redux';
 
-// /**
-//  * Return ShallowWrapper containing node(s) with the given data-test attribute value.
-//  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
-//  * @param {string} val - value of data-test attribute
-//  * @returns {ShallowWrapper}
-//  */
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test attribute value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ * @param {string} val - value of data-test attribute
+ * @returns {ShallowWrapper}
+ */
 
-// export const findByTestAttr = (wrapper, val) => {
-//   return wrapper.find(`[data-test="${val}"]`);
-// };
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
 
-// export const checkProps = (component, conformingProps) => {
-//   const propError = checkPropTypes(
-//     component.proptypes,
-//     conformingProps,
-//     'prop',
-//     component.name,
-//   );
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.proptypes,
+    conformingProps,
+    'prop',
+    component.name,
+  );
 
-//   expect(propError).toBeUndefined();
-// };
+  expect(propError).toBeUndefined();
+};
 
-// export const storeFactory = (initialState) => {
-//   const createStoreWithMiddleware = createStore(
-//     rootReducer,
-//     applyMiddleware(...middlewares),
-//   );
-//   return createStoreWithMiddleware(initialState);
-// };
+export const storeFactory = (initialState) => {
+  const createStoreWithMiddleware = createStore(
+    rootReducer,
+    applyMiddleware(...middlewares),
+  );
+  return createStoreWithMiddleware(initialState);
+};
