4f857f87fb6b15e3c5cc8b39c49370cd4163fc0a
adriandurran <adriandurran@users.noreply.github.com>
2018-08-24 15:15:14 +0100

    chore: added styling to wch events

--- client/src/components/events/WCHEvents.js
+++ client/src/components/events/WCHEvents.js
@@ -1,30 +1,70 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
-import { Item, Header } from 'semantic-ui-react';
+import {
+  Item,
+  Header,
+  Segment,
+  Dimmer,
+  Loader,
+  Label
+} from 'semantic-ui-react';
 
 import { fetchWCHEvents } from '../../actions/events';
 import { selectWCHEventsTicketsSorted } from '../../reducers/selectors/tickets';
+import { removeHTML } from '../../utils/stringManip';
 
 class WCHEvents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loader: true };
+  }
   componentDidMount() {
-    this.props.fetchWCHEvents();
+    this.props.fetchWCHEvents().then(() => this.setState({ loader: false }));
   }
 
   renderEvents() {
     const { events } = this.props;
+    // console.log(events);
+    if (events.length === 0) {
+      return (
+        <Dimmer active={this.state.loader} style={{ height: '6em' }}>
+          <Loader indeterminate>Fetching internal events.......</Loader>
+        </Dimmer>
+      );
+    }
     return events.map((event) => {
       return (
         <Item key={event.id}>
           <Item.Content>
-            <Item.Header>
+            <Item.Header
+              style={
+                event.statustype.id === 8
+                  ? { color: 'red' }
+                  : { color: 'green' }
+              }
+            >
               {event.ticketCustomFields[0].restValue
                 ? event.ticketCustomFields[0].restValue
                 : 'No location given'}
               &nbsp;
               <Moment fromNow>{event.ticketCustomFields[1].restValue}</Moment>
+              {event.statustype.id === 8 && (
+                <span> Still awaiting approval!</span>
+              )}
             </Item.Header>
-            <Item.Meta>{event.id}</Item.Meta>
+            <Item.Meta>
+              Contact: {event.ticketCustomFields[2].restValue}
+            </Item.Meta>
+            <Item.Description>{removeHTML(event.detail)}</Item.Description>
+            <Item.Extra>
+              <Label>
+                Client: {event.clientReporter.firstName}
+                &nbsp;
+                {event.clientReporter.lastName}
+              </Label>
+              <Label>Analyst: {event.clientTech.displayName}</Label>
+            </Item.Extra>
           </Item.Content>
         </Item>
       );
@@ -37,7 +77,11 @@ class WCHEvents extends Component {
         <Header as="h2" textAlign="center">
           WCH Events
         </Header>
-        <Item.Group>{this.renderEvents()}</Item.Group>
+        <Segment>
+          <Item.Group divided relaxed>
+            {this.renderEvents()}
+          </Item.Group>
+        </Segment>
       </div>
     );
   }
